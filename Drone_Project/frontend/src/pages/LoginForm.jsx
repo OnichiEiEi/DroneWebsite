1d3744323b5daee414b48e3fdf0aad2a3c308b0f
OnichiEiEi <mongri2244@gmail.com>
2025-11-05 19:28:25 +0700

    1.2.8

--- Drone_Project/frontend/src/pages/LoginForm.jsx
+++ Drone_Project/frontend/src/pages/LoginForm.jsx
@@ -9,7 +9,7 @@ export default function LoginForm({ onLogin }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3000/auth/login', {
+      const res = await axios.post('https://drone-api-gf69.onrender.com/auth/login', {
         username,
         password
       });
